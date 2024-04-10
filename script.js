6594fc4f20e8db1136b8ca88f5bf2a57f86b6478
illzie <64164811+illzie@users.noreply.github.com>
2024-04-10 12:27:38 -0600

    Added a lot

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+//***** STATE *****//
+let savedNotes = []
+
+
+async function fetchNotes() {
+    const response = await fetch('http://localhost:3000/saved_notes')
+    const fetchedNotes = await response.json()
+    savedNotes = fetchedNotes
+    renderSavedNotes()
+}
+fetchNotes()
+const notesListContainer = document.querySelector('[data-saved-notes]')
+function renderSavedNotes() {
+    for (let i = 0; i < savedNotes.length; i++) {
+        const card = document.createElement('div')
+        card.classList = 'card p-1'
+        card.innerHTML = `
+            <label class="form-control-sm text-secondary text-opacity-50">Created on ${savedNotes[i].dateCreated}
+            <p><strong>Tags:</strong> ${savedNotes[i].tag}</p>
+            </label>
+            <textarea style="border:0;outline:0;" class="card-body form-control-sm bg-white" name="randomID" id="randomID" data-textarea-auto-resize disabled>${savedNotes[i].content}</textarea>
+        `
+        notesListContainer.append(card)
+    }
+}
+
+
 function calcHeight(value) {
     let numberOfLineBreaks = (value.match(/\n/g) || []).length;
     // min-height + lines x line-height + padding + border
@@ -6,7 +33,6 @@ function calcHeight(value) {
 }
 let resizaableTextareas = document.querySelectorAll("[data-textarea-auto-resize]");
 resizaableTextareas.forEach(textarea => {
-    textarea.style.resize = "none";
     textarea.addEventListener("keyup", () => {
         textarea.style.height = calcHeight(textarea.value) + "px";
     });
